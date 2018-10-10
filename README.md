e3a4b237c04a784df3cd7b370dca056810b79f6c
tarepan <tarepan5884@gmail.com>
2018-10-10 09:42:40 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# mnet - Modular neural NETwork library
+mnet (**m**odular neural **net**work library) enables stress-free PyTorch coding with well-structured modules.  
+mnet divides complex neural network codes into common tiny building blocks (modules) and offer them as extendable library.  
+With mnet, users can generate neural network with PyTorch but the stress from code complexity must be reduced!!  
+
+## Amazing code reduction by mnet
+
+## features
+
+## 1-click run with Google Colaboratory
---
+++ main.py
@@ -0,0 +1,65 @@
+# module import rules: https://note.nkmk.me/python-import-usage/ (based on PEP8)
+import time
+
+import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+from tensorboardX import SummaryWriter
+
+from .train import train
+from .test import test
+from .networks.cnn import CNN
+
+class Args:
+  def __init__(self):
+    self.batch_size = 500
+    self.test_batch_size = 1000
+    self.epochs = 10
+    self.lr = 0.01
+    self.momentum = 0.5
+    self.no_cuda = False
+    self.seed = 1
+    self.log_interval = 10
+
+def main():
+    # acquire settings
+    ## with Args lass
+    args = Args()
+
+    # settings
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    # data download and preprocessing for training
+    train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+      transform=transforms.Compose([
+         transforms.ToTensor(),
+         transforms.Normalize((0.1307,), (0.3081,))
+      ])),
+      batch_size=args.batch_size, shuffle=True, **kwargs)
+    # data download and preprocessing for test
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+    # initialization
+    model = CNN().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    writer = SummaryWriter("log")
+#     writer = SummaryWriter("log/run1") # record instance
+
+    # repeating traning and test
+    start = time.time()
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch, writer)
+        test(args, model, device, test_loader, epoch, writer)
+    elapsed_time = time.time() - start
+    print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+if __name__ == "__main__":
+    main()
