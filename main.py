54da4d39547cc25a66c6b29444ae5fca008cd19e
tarepan <tarepan5884@gmail.com>
2018-10-11 09:31:18 +0900

    Refactor by removing unnesesarry comments

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# module import rules: https://note.nkmk.me/python-import-usage/ (based on PEP8)
 import time
-
 import torch
 import torch.optim as optim
 from tensorboardX import SummaryWriter
-
 from .train import train
 from .test import test
 from .networks.cnn import CNN
@@ -12,22 +9,16 @@ from .loaders import getLoaders
 from .getConfigs import getConfigs
 
 def main():
-    # acquire settings
-    args = getConfigs("f")
-
     # settings
+    args = getConfigs("f")
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
-
-    # initialization
+    # initialization & data preprocess
     model = CNN().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     writer = SummaryWriter("log")
-#     writer = SummaryWriter("log/run1") # record instance
-
-    # preprocess data and push them into loaders
     train_loader, test_loader = getLoaders(args, kwargs)
 
     # repeating traning and test
