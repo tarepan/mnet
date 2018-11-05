27318520926f5c73f2cd6c2aee6ebcf7fae5ec50
tarepan <tarepan5884@gmail.com>
2018-11-05 10:51:21 +0900

    Add precise dependencies with setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
-
-setup(name='mnet',
-      version='0.1.0',
-      description='modular networks',
-     )
+from setuptools import setup, find_packages
+setup(
+    name="mnet",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires = numpy; scipy; functional; librosa; pyworld; 
+)
