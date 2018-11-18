2e6aee289882b04b4c7a75d95624317e71674917
Tarepan <tarepan5884@gmail.com>
2018-11-18 19:11:50 +0900

    Fix library name mistake

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name="mnet",
-    version="0.3.0",
+    version="0.4.0",
     packages=find_packages(),
     install_requires = [
         "numpy",
         "scipy",
-        "functional",
+        "pyfunctional",
         "librosa",
         "pyworld"
     ]
